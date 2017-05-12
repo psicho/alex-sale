9ab867c68fabfc705b6050f605287f8bbe0262a0
psicho <9400878@gmail.com>
2017-05-12 15:53:32 +0400

    Доработаны запросы под Python3

--- Aliexpress/alex-sale.py
+++ Aliexpress/alex-sale.py
@@ -3,9 +3,12 @@ from aliexpress_api_client import AliExpress
 aliexpress = AliExpress('17802', '888')
 
 # Получение списка продуктов:
-# products = aliexpress.get_product_list(['productId', 'productTitle', 'salePrice'], keywords=None)
-# for product in products:
-#    print(product['productId'], product['productTitle'], product['salePrice'])
+keywords = 'sony'
+products = aliexpress.get_product_list(['productId', 'productTitle', 'salePrice'], keywords)
+products = products['products']
+for product in products:
+   print('Product Id: ', product['productId'], 'Sale Price', product['salePrice'], 'Product Title: ', product['productTitle'])
+
 
 
 # Получение деталей продукта:
@@ -14,9 +17,9 @@ product = aliexpress.get_product_details(['productId', 'productTitle', 'original
 print(product['productId'], product['productTitle'], product['originalPrice'], product['salePrice'], 'volume = ', product['volume'], product['imageUrl'])
 print('-------')
 
-product1 = aliexpress.get_product_details(['list'], product_id)
-print(product1['list'])
-print('-------')
+# product1 = aliexpress.get_product_details(['list'], product_id)
+# print(product1['list'])
+# print('-------')
 
 # Получение партнерской ссылки на товар:
 urls = ['http://ru.aliexpress.com/', 'http://aliexpress.com/']
